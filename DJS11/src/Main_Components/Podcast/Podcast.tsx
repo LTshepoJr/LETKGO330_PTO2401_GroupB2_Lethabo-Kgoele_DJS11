c46a24cf36af69ed55f1cad3a3e896a29ea9e7e1
Lethabo Kgoele <lethabotshepojr@gmail.com>
2024-10-31 20:34:49 +0200

    added podcast each details

--- DJS11/src/Main_Components/Podcast/Podcast.tsx
+++ DJS11/src/Main_Components/Podcast/Podcast.tsx
@@ -1,13 +1,55 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./Podcast.css";
 
 const Podcast = () => {
   const { id } = useParams();
-  console.log(id);
-  return (
-    <>
-      <h1>This is where the pod details will be</h1>
-    </>
+  const paramsId = id;
+  const [podcast, setPodcast] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoad(true);
+    fetch("https://podcast-api.netlify.app/")
+      .then((prom) => prom.json())
+      .then((data) => setPodcast(data))
+      .catch((err) => setError(err))
+      .finally(() => setLoad(false));
+  }, [id]);
+
+  const filterIdArray = podcast.filter(({ id }) => id === paramsId);
+  function sliceDate<T extends string>(updated: T) {
+    return updated.slice(0, updated.indexOf("T"));
+  }
+  const pod = filterIdArray.map(
+    ({ title, id, description, seasons, updated, image }) => (
+      <div key={id} className="podcastContainer">
+        <div className="podcastImage">
+          <img src={image} alt={`${title} Picture`} />
+          <h3>{title}</h3>
+        </div>
+        <div>
+          <h5>Seasons: {seasons}</h5>
+          <h6>Date Updated: {sliceDate(updated)}</h6>
+        </div>
+        <div>
+          <h2>About</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    )
   );
+
+  if (load) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1 className="jsonError">Something went wrong!!</h1>;
+  }
+
+  return <>{pod}</>;
 };
 
 export default Podcast;
